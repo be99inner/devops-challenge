1d981a6a7c787b95c40b5c42b0b8fefff5a775c8
be99inner <anurak.jann@gmail.com>
2026-01-02 07:32:25 +0700

    fix: change from static code to terraform variables

--- terraform/environments/sandbox/main.tf
+++ terraform/environments/sandbox/main.tf
@@ -3,11 +3,11 @@ module "vpc" {
   version = "~> 5.0"
 
   name = "${var.environment}-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr
 
   azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
